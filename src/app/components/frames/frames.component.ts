2dc4da9a500fa3c3bb2d6c55ddef549d8675ea8e
Manikanta Tankala <im.manikanta@gmail.com>
2019-03-10 09:35:51 +0800

    frame component

--- src/app/components/frames/frames.component.ts
+++ src/app/components/frames/frames.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-frames',
@@ -6,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frames.component.scss']
 })
 export class FramesComponent implements OnInit {
-
-  constructor() { }
+  url: any;
+  type: string;
+  constructor(public route: ActivatedRoute, private sanitizer: DomSanitizer) {
+    this.route.params.subscribe(params => {
+      console.log(params.type);
+      this.type = params.type;
+      switch (params.type) {
+        case 'cards':
+          this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.dbs.com.sg/personal/promotion/cards-signup');
+          break;
+        case 'currency':
+          this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.dbs.com.sg/personal/rates-online/foreign-currency-foreign-exchange.page');
+          break;
+        case 'contact':
+          this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.dbs.com.sg/personal/support/home.html?pid=sg-dbs-pweb-header-default-help-support-textlink');
+          break;
+      }
+    });
+  }
 
   ngOnInit() {
   }
