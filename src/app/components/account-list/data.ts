6225470e429d19d3c069639bec47c4a38bb95289
Manikanta Tankala <im.manikanta@gmail.com>
2019-03-11 21:51:43 +0800

    add new data and fix assets problem

---
+++ src/app/components/account-list/data.ts
@@ -0,0 +1,9 @@
+export const data = [{
+    'accountType': 'DBS Unlimited cashback',
+    'accountId': 'DBS 123',
+    'amount': '3000'
+}, {
+    'accountType': 'DBS Rewards',
+    'accountId': 'DBS 897',
+    'amount': '1000'
+}];
